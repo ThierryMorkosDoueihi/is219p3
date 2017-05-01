353f39931f7255fe29b65cd3608492f2491ba238
ThierryMorkosDoueihi <thierrydouaihy@gmail.com>
2017-05-01 13:54:25 -0400

    Create the needed functions
    like and dislike functions increment the original values of likes and
    dislikes repectively, of the specific movie.
    timeText function fixes the format of the runtime so it displays in
    hours and minutes and return the value.

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -108,13 +108,26 @@ app.controller('HomeController', ['$scope', function($scope) {
 	
 	
 	/* ADD FUNCTIONS FOR STEP 7 HERE */
-		
+	$scope.like = function(index){
+		$scope.movies[index].likes++;
+	};
 	
+	$scope.dislike = function(index){
+		$scope.movies[index].dislikes++;
+	};	
 	
+	$scope.posterClick = function(index){
+		$scope.movies[index].posterindex++;
+		if($scope.movies[index].posterindex >= $scope.movies[index].posters.length){
+			$scope.movies[index].posterindex = 0;
+		};
+	};
 	
-	
-	
-	
-	
+	$scope.timeText = function(minutes){
+		hours = Math.floor(minutes/60);
+		rem = minutes%60;
+		retVal = hours.toString() + "h " + rem.toString() + "m";
+		return(retVal);
+	};
 	
 }]);
